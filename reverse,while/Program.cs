81250a78006e4c05e92465b6f419acf77096002b
AkshitaRahangdale <akshitarahangdale@1720gmail.com>
2022-10-06 13:16:20 +0530

    Add project files.

---
+++ reverse,while/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 100;
+            while(i>=50)
+            {
+                Console.WriteLine("hii");
+                i--;
+            }
+        }
+    }
+}
